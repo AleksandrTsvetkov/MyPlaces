ff04f043893fa763cb9534545698d644af524399
Alexander <aleksandrcw@gmail.com>
2020-01-10 18:17:55 +0300

    Custom Table View Cell

---
+++ MyPlaces/MainTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  MainTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Александр Цветков on 10.01.2020.
+//  Copyright © 2020 Александр Цветков. All rights reserved.
+//
+
+import UIKit
+
+class MainTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
